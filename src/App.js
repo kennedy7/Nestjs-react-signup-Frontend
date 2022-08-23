b21a9813e051f4a1c6ecd3e5c43f58433e6e6865
kennedy7 <kennedyhillary6@gmail.com>
2022-08-23 21:42:19 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername ] = useState('')
+  const [email, setEmail ] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <p>Username</p>
+        <input 
+        required 
+        type="text" 
+        value = "username"
+        onChange={(e)=>{
+          setUsername(e.target.value)
+        }}
+        />
+
+      <p>Email</p>
+        <input 
+        required 
+        type="text" 
+        value = "email"
+        onChange={(e)=>{
+          setEmail(e.target.value)
+        }}
+
+
+        />
+        </form>
+    
     </div>
   );
 }
